82aea8fb3d850d888bda76ea74350a66265bf8c4
Kangcheng Xu <kxu@redhat.com>
2022-12-20 14:38:43 -0500

    WIP: add uses directives by static analysis

--- src/jdk.jlink/share/classes/jdk/tools/jlink/internal/plugins/ClassPathPlugin.java
+++ src/jdk.jlink/share/classes/jdk/tools/jlink/internal/plugins/ClassPathPlugin.java
@@ -3,28 +3,45 @@ package jdk.tools.jlink.internal.plugins;
 import com.sun.tools.jdeps.JdepsConfiguration;
 import com.sun.tools.jdeps.ModuleInfoBuilder;
 import jdk.internal.opt.CommandLine;
+import jdk.internal.org.objectweb.asm.ClassReader;
 import jdk.internal.org.objectweb.asm.ClassWriter;
 import jdk.internal.org.objectweb.asm.ModuleVisitor;
 import jdk.internal.org.objectweb.asm.Opcodes;
+import jdk.internal.org.objectweb.asm.Type;
+import jdk.internal.org.objectweb.asm.tree.AbstractInsnNode;
+import jdk.internal.org.objectweb.asm.tree.ClassNode;
+import jdk.internal.org.objectweb.asm.tree.LdcInsnNode;
+import jdk.internal.org.objectweb.asm.tree.MethodInsnNode;
 import jdk.tools.jlink.internal.Archive;
+import jdk.tools.jlink.internal.ModularJarArchive;
 import jdk.tools.jlink.plugin.ResourcePool;
 import jdk.tools.jlink.plugin.ResourcePoolBuilder;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.UncheckedIOException;
 import java.lang.module.ModuleDescriptor;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Set;
 import java.util.function.Function;
 
 public class ClassPathPlugin extends AbstractPlugin {
     private static final String NAME = "class-path";
     private static final String JAVA_BASE_MOD = "java.base";
+    private static final String CLASS_EXT = ".class";
+    private static final String TEMP_DIR = "jlink-class-path-plugin-tmp";
     private static final String[] MODULE_PATH_OPTIONS = {"--module-path", "-p"};
 
     private static final List<Path> classPath = new ArrayList<>();
@@ -129,38 +146,112 @@ public class ClassPathPlugin extends AbstractPlugin {
         // generator module descriptors with jdeps
         List<ModuleDescriptor> descriptors;
         try (JdepsConfiguration config = builder.build()) {
-            Path tmp = Files.createTempDirectory("jlink-class-path-plugin-tmp");
-            tmp.toFile().deleteOnExit();
+            File tmp = Files.createTempDirectory(TEMP_DIR).toFile();
+            tmp.deleteOnExit();
             ModuleInfoBuilder miBuilder = new ModuleInfoBuilder(
                     config,
                     classPath.stream().map(Path::toString).toList(),
-                    tmp, // not used
+                    tmp.toPath(), // not used
                     false);
             miBuilder.run(true, null, true);
+            tmp.delete();
             descriptors = miBuilder.descriptors().toList();
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
 
         // TODO: verify classpath and descriptors are one-to-one mappings and the order is the same
-
         List<Archive> archives = new ArrayList<>();
+        for (int i = 0; i < descriptors.size(); i++) {
+            ModuleDescriptor descriptor = descriptors.get(i);
+            Path path = classPath.get(i);
+            ModularJarArchive archive = new ModularJarArchive(descriptor.name(), path, version[0]);
 
-//        descriptors.forEach(...);
+            PatchedModuleDirectives directives = new PatchedModuleDirectives(descriptor);
+            stripNonExistingProvideClauses(descriptor, archive);
+            addUseClauses(directives, archive);
+
+            archives.add(archive);
+        }
 
         in.transformAndCopy(Function.identity(), out);
 
+        // TODO: add generated module resources to pool
+//        out.add(...);
 
-//        out.add(ResourcePoolEntryFactory.create());
 
         return out.build();
     }
 
-    private void stripNonExistingProvideClauses(ModuleDescriptor descriptor) {
+    // TODO: is this even necessary if descriptors are not generated with javac?
+    //       a non-existent SPI could be available in another module
+    private void stripNonExistingProvideClauses(ModuleDescriptor descriptor, ModularJarArchive archive) {
 
     }
 
-    private byte[] compileModuleInfo(ModuleDescriptor descriptor) {
+    private void addUseClauses(PatchedModuleDirectives directives, ModularJarArchive archive) {
+        // Scan for ServiceLoader.load() calls
+        archive.entries().forEach(entry -> {
+            if (entry.type() != Archive.Entry.EntryType.CLASS_OR_RESOURCE || !entry.name().endsWith(CLASS_EXT)) {
+                return;
+            }
+
+            try (InputStream is = entry.stream()) {
+                ClassReader cr = new ClassReader(is.readAllBytes());
+                ClassNode cn = new ClassNode();
+                cr.accept(cn, ClassReader.EXPAND_FRAMES);
+
+                cn.methods.forEach(method -> {
+                    AbstractInsnNode[] lastTwoInstructions = new AbstractInsnNode[2];
+                    int[] i = new int[]{0};
+
+                    method.instructions.forEach(instruction -> {
+                        lastTwoInstructions[i[0]++ % 2] = instruction;
+
+                        // FIXME: Argument could be already on stack. How to do data-flow analysis properly?
+                        if (instruction instanceof MethodInsnNode invocation) {
+                            // one argument variant: ServiceLoader.load(Class):ServiceLoader
+                            if (invocation.getOpcode() == Opcodes.INVOKESTATIC
+                                    && invocation.name.equals("load")
+                                    && invocation.owner.equals("java/util/ServiceLoader")
+                                    && invocation.desc.equals("(Ljava/lang/Class;)Ljava/util/ServiceLoader;")) {
+                                if (lastTwoInstructions[0] instanceof LdcInsnNode ldc) {
+                                    if (ldc.cst instanceof Type type) {
+                                        if (type.getSort() == Type.OBJECT) {
+//                                            System.out.println("uses " + type.getClassName());
+                                            try {
+                                                directives.uses().add(type.getClassName());
+                                            } catch (Exception e) {
+                                                e.printStackTrace();
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+
+                            // two argument variant: ServiceLoader.load(Class, ClassLoader):ServiceLoader
+                            if (invocation.getOpcode() == Opcodes.INVOKESTATIC
+                                    && invocation.name.equals("load")
+                                    && invocation.owner.equals("java/util/ServiceLoader")
+                                    && invocation.desc.equals("(Ljava/lang/Class;Ljava/lang/ClassLoader;)Ljava/util/ServiceLoader;")) {
+                                // TODO: the loading classloader argument to stack might be non-trivial like LDC
+                            }
+
+                            // We don't care about loading services with ModuleLayer. Legacy code don't use it anyway.
+                            return;
+                        }
+
+
+                    });
+                });
+
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        });
+    }
+
+    private byte[] compileModuleInfo(ModuleDescriptor descriptor, PatchedModuleDirectives directives) {
         ClassWriter cw = new ClassWriter(0);
         cw.visit(Opcodes.V19, Opcodes.ACC_MODULE, "module-info", null, null, null);
 
@@ -168,7 +259,7 @@ public class ClassPathPlugin extends AbstractPlugin {
 
         ModuleVisitor mv = cw.visitModule(descriptor.name(), 0, null);
 
-        for (ModuleDescriptor.Requires require : descriptor.requires()) {
+        for (ModuleDescriptor.Requires require : directives.requires()) {
             mv.visitRequire(require.name(),
                     require.name().equals(JAVA_BASE_MOD)
                             ? Opcodes.ACC_MANDATED
@@ -176,23 +267,23 @@ public class ClassPathPlugin extends AbstractPlugin {
                     require.rawCompiledVersion().orElse(null));
         }
 
-        for (ModuleDescriptor.Exports export : descriptor.exports()) {
+        for (ModuleDescriptor.Exports export : directives.exports()) {
             mv.visitExport(classToInternalName(export.source()),
                     exportModifiersToMask(export.modifiers()),
                     export.targets().stream().map(this::classToInternalName).toArray(String[]::new));
         }
 
-        for (ModuleDescriptor.Opens open : descriptor.opens()) {
+        for (ModuleDescriptor.Opens open : directives.opens()) {
             mv.visitOpen(open.source(),
                     openModifiersToMask(open.modifiers()),
                     open.targets().stream().map(this::classToInternalName).toArray(String[]::new));
         }
 
-        for (String use : descriptor.uses()) {
+        for (String use : directives.uses()) {
             mv.visitUse(classToInternalName(use));
         }
 
-        for (ModuleDescriptor.Provides provide : descriptor.provides()) {
+        for (ModuleDescriptor.Provides provide : directives.provides()) {
             mv.visitProvide(classToInternalName(provide.service()),
                     provide.providers().stream().map(this::classToInternalName).toArray(String[]::new));
         }
@@ -245,5 +336,72 @@ public class ClassPathPlugin extends AbstractPlugin {
 
     private String classToInternalName(String className) {
         return className.replace('.', '/');
+    }
+
+    private static class PatchedModuleDirectives {
+        private final ModuleDescriptor descriptor;
+        private final Set<ModuleDescriptor.Requires> requires = new HashSet<>();
+        private final Set<ModuleDescriptor.Exports> exports = new HashSet<>();
+        private final Set<ModuleDescriptor.Opens> opens = new HashSet<>();
+        private final Set<String> uses = new HashSet<>();
+        private final Set<ModuleDescriptor.Provides> provides = new HashSet<>();
+
+        private PatchedModuleDirectives() {
+            descriptor = null;
+        }
+
+        private PatchedModuleDirectives(ModuleDescriptor descriptor) {
+            super();
+            this.descriptor = descriptor;
+            this.requires.addAll(descriptor.requires());
+            this.exports.addAll(descriptor.exports());
+            this.opens.addAll(descriptor.opens());
+            this.uses.addAll(descriptor.uses());
+            this.provides.addAll(descriptor.provides());
+        }
+
+        public void addRequires(ModuleDescriptor.Requires require) {
+            requires.stream().filter(r -> r.name().equals(require.name())).findAny().ifPresent(requires::remove);
+            requires.add(require);
+        }
+
+        public Set<ModuleDescriptor.Requires> requires() {
+            return requires;
+        }
+
+        public void addExports(ModuleDescriptor.Exports export) {
+            exports.stream().filter(e -> e.source().equals(export.source())).findAny().ifPresent(exports::remove);
+            exports.add(export);
+        }
+
+        public Set<ModuleDescriptor.Exports> exports() {
+            return exports;
+        }
+
+        public void addOpens(ModuleDescriptor.Opens open) {
+            opens.stream().filter(o -> o.source().equals(open.source())).findAny().ifPresent(opens::remove);
+            opens.add(open);
+        }
+
+        public Set<ModuleDescriptor.Opens> opens() {
+            return opens;
+        }
+
+        public void addUses(String use) {
+            uses.add(use);
+        }
+
+        public Set<String> uses() {
+            return uses;
+        }
+
+        public void addProvides(ModuleDescriptor.Provides provide) {
+            provides.stream().filter(p -> p.service().equals(provide.service())).findAny().ifPresent(provides::remove);
+            provides.add(provide);
+        }
+
+        public Set<ModuleDescriptor.Provides> provides() {
+            return provides;
+        }
     }
 }
